58f97cb195112a72cd53103b1eefb750037551f0
Tiago Vinícius <tiagovinicyus1996@gmail.com>
2023-11-21 13:54:57 +0000

    update

--- project_xxx/src/components/Header.jsx
+++ project_xxx/src/components/Header.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
 import '../style/Home.css'
+
 
 const Header = () => {
 
@@ -20,13 +21,42 @@ const Header = () => {
         menuNav.classList.toggle('opened')
     }
 
+
     function searchItem() {
         const header = document.getElementById('header')
         const searchItem = document.getElementById('searchBox')
+        const showFiltersButton = document.getElementById('showFilters')
+        const filter = document.getElementById('filter')
+
 
         header.classList.toggle('expand')
         searchItem.classList.toggle('visible')
+        if (searchItem.className == 'visible') {
+            showFiltersButton.style.top = '70px'
+            filter.style.top = '125px'
+        } else {
+            showFiltersButton.style.top = '40px'
+            filter.style.top = '95px'
+        }
         
+        
+    }
+    
+    
+    
+    const handleSubmit = (event) => {
+        event.preventDefault();}
+
+    function showFilters() {
+        const filter = document.getElementById('filter')
+
+        filter.classList.toggle('showFilters')
+    }
+
+    function searchJob() {
+        const filter = document.getElementById('filter')
+
+        filter.classList.toggle('showFilters')
     }
 
   return (
@@ -37,9 +67,86 @@ const Header = () => {
                 Project X
             </h1>
             <div id='searchBox'>
-                <input type="search" name="searchItem" id="searchItem" placeholder='O que estás a procurar?'/>
+                <input type="search" name="searchItem" id="searchItem" placeholder='Qual emprego estás a procurar?'/>
             </div>
             <button className='material-symbols-outlined' id='searchButton' onClick={searchItem}>search</button>
+            <button id='showFilters' onClick={showFilters}>Filters <span className='material-symbols-outlined' id='filterSymbol'>tune</span></button>
+            <div id='filter'>
+                <form onSubmit={handleSubmit}>
+                    <input type="search" name="" id="" placeholder='Em qual cidade?'/>   
+                <div>
+                    <select name="" id="">
+                            <optgroup>
+                                <option value="">Mais Recentes</option>
+                                <option value="">Últimas 24h</option>
+                                <option value="">Última Semana</option>
+                                <option value="">Mais Antigos</option>   
+                            </optgroup>
+                    </select>
+                </div>
+                <div>
+                    <fieldset id='jobsType'>
+                    <legend>Tipos de Vaga</legend>
+                        <label>
+                        <input type="checkbox" name="categoria" value="all"/> Tudo
+                        </label>
+                        <label>
+                        <input type="checkbox" name="categoria" value="full-time"/> Full-Time
+                        </label>
+                        <label>
+                        <input type="checkbox" name="categoria" value="part-time"/> Part-Time
+                        </label>
+                        <label>
+                        <input type="checkbox" name="categoria" value="internship"/> Estágio
+                        </label>
+                    </fieldset>
+                </div>
+                <div>
+                    <fieldset id='contractType'>
+                    <legend>Tipos de Contrato</legend>
+                        <label>
+                        <input type="checkbox" name="categoria" value="all"/> Tudo
+                        </label>
+                        <label>
+                        <input type="checkbox" name="categoria" value="undefined"/> A Definir
+                        </label>
+                        <label>
+                        <input type="checkbox" name="categoria" value="temporary"/> Temporário
+                        </label>
+                        <label>
+                        <input type="checkbox" name="categoria" value="without-term"/> Sem Termo
+                        </label>
+                        <label>
+                        <input type="checkbox" name="categoria" value="with-term"/> A Termo
+                        </label>
+                    </fieldset>
+                </div>
+                <div>
+                    <fieldset>
+                    <legend>Salário</legend>
+                        <label>
+                        <input type="checkbox" name="categoria" value="all"/> Tudo
+                        </label>
+                        <label>
+                        <input type="checkbox" name="categoria" value="until-500"/> Até 500€
+                        </label>
+                        <label>
+                        <input type="checkbox" name="categoria" value="500-1k"/> 500€ - 1000€
+                        </label>
+                        <label>
+                        <input type="checkbox" name="categoria" value="1k-1_5k"/> 1000€ - 1500€
+                        </label>
+                        <label>
+                        <input type="checkbox" name="categoria" value="1_5k-2k"/> 1500€ - 2000€
+                        </label>
+                        <label>
+                        <input type="checkbox" name="categoria" value="plus-2k"/> mais de 2000€
+                        </label>
+                    </fieldset>
+                </div>
+                    <input type="submit" value="Buscar Vagas" id='searchJobButton' onClick={searchJob}/>
+                </form>
+            </div>
         </header>
         <nav id='menuNavigation'>
             <button id='closeMenu' className='material-symbols-outlined' onClick={closeMenu}>close</button>
